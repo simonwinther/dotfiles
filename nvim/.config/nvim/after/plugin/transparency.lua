109180b9b50a97a565d2d81c0148579cb1014003
Simon Winther <simonwa01@gmail.com>
2025-12-09 01:15:16 +0100

    fix(snacks.gh): snacks.gh didn't work because of one fkn line, now it does xD

--- nvim/.config/nvim/after/plugin/transparency.lua
+++ nvim/.config/nvim/after/plugin/transparency.lua
@@ -1,5 +1,4 @@
 -- Transparent background
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
