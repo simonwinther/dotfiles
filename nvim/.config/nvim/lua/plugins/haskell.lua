bf36ac2754534ce4535490cf1c6e67a5189324e7
Simon Winther <simonwa01@gmail.com>
2025-12-10 04:31:38 +0100

    waybar, haskell, and walker

--- nvim/.config/nvim/lua/plugins/haskell.lua
+++ nvim/.config/nvim/lua/plugins/haskell.lua
@@ -1,62 +1,11 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "haskell" } },
-  },
-  {
-    "mrcjkb/haskell-tools.nvim",
-    version = "^6",
-    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
-    init = function()
-      vim.g.haskell_tools = {
-        hls = {
-          cmd = { "haskell-language-server-wrapper", "--lsp" },
-        },
-      }
-    end,
-  },
-  {
-    "mason-org/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "haskell-debug-adapter" })
-    end,
-  },
-  {
-    "mrcjkb/haskell-snippets.nvim",
-    dependencies = { "L3MON4D3/LuaSnip" },
-    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
-    config = function()
-      local haskell_snippets = require("haskell-snippets").all
-      require("luasnip").add_snippets("haskell", haskell_snippets, { key = "haskell" })
-    end,
-  },
+  -- Simon, haskell-tools er hls, så vi skal slå hls client fra nvim-lspconfig,
+  -- siden haskell-tools.nvim allerede starter HLS, ellers ender vi med 2 af alt ... xD
   {
     "neovim/nvim-lspconfig",
     opts = {
-      setup = {
-        hls = function()
-          return true
-        end,
-      },
-    },
-  },
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        haskell = { "fourmolu" },
-        cabal = { "cabal_fmt" },
-      },
-    },
-  },
-  {
-    "mfussenegger/nvim-lint",
-    optional = true,
-    opts = {
-      linters_by_ft = {
-        haskell = { "hlint" },
+      servers = {
+        hls = false,
       },
     },
   },
