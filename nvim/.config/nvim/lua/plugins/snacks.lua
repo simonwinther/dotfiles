2af56e4fb49479a0ed35583a5db0a34e1640aac5
Simon Winther <simonwa01@gmail.com>
2025-12-21 08:06:59 +0100

    remove keybind snacks:)

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -179,23 +179,6 @@ return {
       explorer = { enabled = false }, -- disable built-in explorer (i use neotree)
       picker = {
         enabled = true,
-        win = {
-          input = {
-            keys = {
-              ["-"] = { "edit_split", mode = { "n" } }, -- horizontal split
-              ["|"] = { "edit_vsplit", mode = { "n" } }, -- vertical split
-              ["<C-t>"] = { "tabedit", mode = { "n" } }, -- new tab (optional)
-            },
-          },
-          -- If you also want the same keys to work when the list has focus:
-          list = {
-            keys = {
-              ["-"] = { "edit_split", mode = { "n" } },
-              ["|"] = { "edit_vsplit", mode = { "n" } },
-              ["<C-t>"] = { "tabedit", mode = { "n" } },
-            },
-          },
-        },
       },
     },
   },
