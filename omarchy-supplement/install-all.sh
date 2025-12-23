8d688801fb341ce47651d7610ef354c6167ae46d
Simon Winther <simonwa01@gmail.com>
2025-12-24 00:35:05 +0100

    install-all

---
+++ omarchy-supplement/install-all.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+./install-firefox.sh
+./install-hyprland-overrides.sh
+./install-ghostty.sh
+./install-stow.sh
+./install-texlive.sh
+./install-tmux.sh
+./install-dotfiles.sh
+./install-zsh.sh
+./set-shell.sh
