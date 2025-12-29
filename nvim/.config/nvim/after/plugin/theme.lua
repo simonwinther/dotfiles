14e55c1368f3d168d796760b6cb4a7afab6afe16
Simon Winther <simonwa01@gmail.com>
2025-12-29 15:03:19 +0100

    cursorline and linenr color more bright

--- nvim/.config/nvim/after/plugin/theme.lua
+++ nvim/.config/nvim/after/plugin/theme.lua
@@ -1,1 +1,4 @@
 vim.api.nvim_set_hl(0, "FlashLabel", { bg = "#74c7ec", fg = "#11111b", bold = true })
+
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#7aa2f7", bold = true })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#a9b1d6", bold = false })
