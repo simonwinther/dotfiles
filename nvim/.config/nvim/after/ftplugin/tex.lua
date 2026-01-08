c511213f9e27842a4a125bc67bfd0aecda05065c
Simon Winther <simonwa01@gmail.com>
2026-01-08 15:52:21 +0100

    moved cpp and tex specific stuff into their ftplugin folder

--- nvim/.config/nvim/after/ftplugin/tex.lua
+++ nvim/.config/nvim/after/ftplugin/tex.lua
@@ -1,19 +1,59 @@
 -- ~/.config/nvim/after/ftplugin/tex.lua
--- This only runs in LaTeX files (.tex)
-local ns = require("nvim-surround")
+-- LaTeX Specific Configuration
 
-ns.buffer_setup({
-  aliases = {
-    ["b"] = false,
-  },
-  surrounds = {
-    e = { add = { "\\emph{", "}" } },
-    b = { add = { "\\textbf{", "}" } },
-    i = { add = { "\\textit{", "}" } },
-    u = { add = { "\\underline{", "}" } },
-    c = { add = { "\\cite{", "}" } },
-    m = { add = { "\\(", "\\)" } },
-    d = { add = { "\\[", "\\]" } },
-    t = { add = { "\\texttt{", "}" } },
-  },
+--------------------------------------------------------------------------------
+-- KEYMAPS
+--------------------------------------------------------------------------------
+-- Paste Image (img-clip.nvim)
+vim.keymap.set("n", "<leader>i", "<cmd>PasteImage<cr>", {
+  desc = "[LaTeX] Paste image from system clipboard",
+  buffer = true,
 })
+
+--------------------------------------------------------------------------------
+-- NVIM-SURROUND SETUP
+--------------------------------------------------------------------------------
+-- Use pcall to safely check if nvim-surround is installed
+local surround_available, ns = pcall(require, "nvim-surround")
+
+if surround_available then
+  ns.buffer_setup({
+    aliases = { ["b"] = false }, -- Disable default 'b' alias to use bold
+    surrounds = {
+      e = { add = { "\\emph{", "}" } },
+      b = { add = { "\\textbf{", "}" } },
+      i = { add = { "\\textit{", "}" } },
+      u = { add = { "\\underline{", "}" } },
+      c = { add = { "\\cite{", "}" } },
+      t = { add = { "\\texttt{", "}" } },
+      m = { add = { "\\(", "\\)" } }, -- Inline math
+      d = { add = { "\\[", "\\]" } }, -- Display math
+    },
+  })
+end
+
+--------------------------------------------------------------------------------
+-- SWITCH.NVIM CONFIGURATION
+--------------------------------------------------------------------------------
+-- Define custom toggle pairs for LaTeX
+local latex_switches = {
+  -- Environments
+  { [[\\begin{itemize}]], [[\\begin{enumerate}]], [[\\begin{description}]] },
+  { [[\\end{itemize}]], [[\\end{enumerate}]], [[\\end{description}]] },
+
+  -- Sections
+  { [[\\section]], [[\\subsection]], [[\\subsubsection]] },
+
+  -- Math Environments
+  { [[\\begin{equation}]], [[\\begin{align}]], [[\\begin{gather}]] },
+  { [[\\end{equation}]], [[\\end{align}]], [[\\end{gather}]] },
+}
+
+-- Merge with existing switches safely
+local current_defs = vim.b.switch_custom_definitions or {}
+
+for _, def in ipairs(latex_switches) do
+  table.insert(current_defs, def)
+end
+
+vim.b.switch_custom_definitions = current_defs
