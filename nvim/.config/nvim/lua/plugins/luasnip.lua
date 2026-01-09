41edafd74e3e60a4a86085f28788d0ea409b480a
Simon Winther <simonwa01@gmail.com>
2026-01-09 21:11:32 +0100

    fixed luasnip and made it work with new keybinds

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -3,33 +3,56 @@ return {
     "L3MON4D3/LuaSnip",
     event = "InsertEnter",
     dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+      end,
+    },
+
+    opts = {
+      history = true,
+      delete_check_events = "TextChanged",
+    },
+
+    keys = {
       {
-        "rafamadriz/friendly-snippets",
-        config = function()
-          require("luasnip.loaders.from_vscode").lazy_load()
-          require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+        "<C-J>",
+        function()
+          local ls = require("luasnip")
+          if ls.expand_or_jumpable() then
+            ls.expand_or_jump()
+          end
         end,
+        mode = { "i", "s" },
+        silent = true,
+      },
+
+      -- 2. JUMP BACKWARD
+      {
+        "<C-K>",
+        function()
+          local ls = require("luasnip")
+          if ls.jumpable(-1) then
+            ls.jump(-1)
+          end
+        end,
+        mode = { "i", "s" },
+        silent = true,
+      },
+
+      -- 3. CHANGE CHOICE (For snippets with multiple options)
+      {
+        "<C-E>",
+        function()
+          local ls = require("luasnip")
+          if ls.choice_active() then
+            ls.change_choice(1)
+          end
+        end,
+        mode = { "i", "s" },
+        silent = true,
       },
     },
-    opts = function()
-      local ls = require("luasnip")
-      LazyVim.cmp.actions.snippet_forward = function()
-        if ls.jumpable(1) then
-          ls.jump(1)
-          return true
-        end
-      end
-      LazyVim.cmp.actions.snippet_stop = function()
-        if ls.expand_or_jumpable() then
-          ls.unlink_current()
-          return true
-        end
-      end
-
-      return {
-        history = true,
-        delete_check_events = "TextChanged",
-      }
-    end,
   },
 }
