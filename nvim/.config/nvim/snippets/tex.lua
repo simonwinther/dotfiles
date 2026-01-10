ad99445dedb24e7e63581ad8565c409558a324bc
Simon Winther <simonwa01@gmail.com>
2026-01-10 19:20:11 +0100

    snippets for latex :)

--- nvim/.config/nvim/snippets/tex.lua
+++ nvim/.config/nvim/snippets/tex.lua
@@ -1,15 +1,77 @@
 local ls = require("luasnip")
-local s, t, i, fmt = ls.snippet, ls.text_node, ls.insert_node, require("luasnip.extras.fmt").fmt
+local s = ls.snippet
+local i = ls.insert_node
+local f = ls.function_node
+local fmt = require("luasnip.extras.fmt").fmt
 
-local function in_math()
-  return vim.fn["vimtex#syntax#in_mathzone"]() == 1
+----------------------------------------
+--- HELPER FUNCTIONS
+----------------------------------------
+
+-- Helper function: Converts "My Title" -> "my_title"
+local function to_label(args)
+  -- args[1][1] is the text content of the first node linked to this function
+  return args[1][1]:lower():gsub("%p", ""):gsub("%s+", "_")
 end
 
 return {
-  -- \section
-  s("sec", fmt("\\section{{{}}}\n\\label{{sec:{}}}\n", { i(1, "Title"), i(2, "key") })),
-  -- \subsection
-  s("ssec", fmt("\\subsection{{{}}}\n\\label{{ssec:{}}}\n", { i(1, "Title"), i(2, "key") })),
-  -- inline math (only in math zone)
-  s({ trig = "mm", dscr = "inline math", condition = in_math }, fmt([[\( {}\)]], { i(1) })),
+  -- SECTION
+  s(
+    "sec",
+    fmt(
+      [[
+    \section{{{title}}}\label{{sec:{lbl}}} % (fold)
+
+    {final}
+
+    % section {lbl_mirror} (end)
+    ]],
+      {
+        title = i(1, "Title"),
+        lbl = f(to_label, { 1 }),
+        final = i(0),
+        lbl_mirror = f(to_label, { 1 }),
+      }
+    )
+  ),
+
+  -- SUBSECTION
+  s(
+    "sub",
+    fmt(
+      [[
+    \subsection{{{title}}}\label{{sub:{lbl}}} % (fold)
+
+    {final}
+
+    % subsection {lbl_mirror} (end)
+    ]],
+      {
+        title = i(1, "Title"),
+        lbl = f(to_label, { 1 }),
+        final = i(0),
+        lbl_mirror = f(to_label, { 1 }),
+      }
+    )
+  ),
+
+  -- SUBSUBSECTION
+  s(
+    "ssub",
+    fmt(
+      [[
+    \subsubsection{{{title}}}\label{{ssub:{lbl}}} % (fold)
+
+    {final}
+
+    % subsubsection {lbl_mirror} (end)
+    ]],
+      {
+        title = i(1, "Title"),
+        lbl = f(to_label, { 1 }),
+        final = i(0),
+        lbl_mirror = f(to_label, { 1 }),
+      }
+    )
+  ),
 }
