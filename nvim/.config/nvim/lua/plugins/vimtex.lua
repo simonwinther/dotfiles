c4b125a456630a5fa1f3c5522d82bdaeb0c9fa41
Simon Winther <simonwa01@gmail.com>
2026-01-15 21:46:22 +0100

    lazy load: vimtex

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -1,17 +1,15 @@
 return {
   "lervag/vimtex",
-  lazy = false, -- load immediately
+  lazy = true, -- override
+  ft = { "tex", "plaintex" },
   init = function()
     -- Use Zathura as PDF viewer
     vim.g.vimtex_view_method = "zathura"
-
-    -- Compile automatically using latexmk
     vim.g.vimtex_compiler_method = "latexmk"
 
-    -- Optional: prevent auto-opening quickfix window
+    -- prevent auto-opening quickfix window
     vim.g.vimtex_quickfix_mode = 0
 
-    -- Optional: use a local build directory
     vim.g.vimtex_compiler_latexmk = {
       build_dir = "build",
       callback = 1,
