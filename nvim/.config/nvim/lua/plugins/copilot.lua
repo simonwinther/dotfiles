5f49e737da3e30e8225e562605173ac70e9fe2f5
Simon Winther <simonwa01@gmail.com>
2026-01-18 16:49:19 +0100

    copilot simplified

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -2,41 +2,22 @@ return {
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
-    event = "InsertEnter",
     build = ":Copilot auth",
-    opts = function()
-      LazyVim.cmp.actions.ai_accept = function()
-        local suggestion = require("copilot.suggestion")
-        if suggestion.is_visible() then
-          LazyVim.create_undo()
-          suggestion.accept()
-          return true
-        end
-      end
-
-      return {
-        suggestion = {
-          enabled = true,
-          auto_trigger = false,
-          keymap = {
-            accept = false,
-            accept_word = "<C-l>",
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-_>",
-          },
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = false,
+        keymap = {
+          accept = false,
+          accept_word = "<C-l>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-_>",
         },
-        panel = {
-          enabled = true,
-          auto_refresh = true,
-          keymap = {
-            open = "<M-CR>",
-          },
-          layout = { position = "bottom", ratio = 0.4 },
-        },
-        filetypes = { markdown = true, help = true },
-      }
-    end,
+      },
+      filetypes = { markdown = true, help = true },
+    },
   },
   {
     "neovim/nvim-lspconfig",
