caacf1d25b0b91edab014a481787ae1f08816742
Simon Winther <simonwa01@gmail.com>
2026-01-25 03:49:07 +0100

    tweesitter:added nix and hyprlang

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,6 +2,6 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     local ensure_installed = opts.ensure_installed
-    vim.list_extend(ensure_installed, { "latex", "bibtex" })
+    vim.list_extend(ensure_installed, { "latex", "bibtex", "nix", "hyprlang" })
   end,
 }
