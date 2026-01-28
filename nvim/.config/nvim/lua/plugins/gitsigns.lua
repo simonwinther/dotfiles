28ab807c29a5ae613ba717f01609bc5f1de254a8
Simon Winther <simonwa01@gmail.com>
2026-01-28 23:39:21 +0100

    bigger gitsigns, might go back to small

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,22 @@
 return {
   "lewis6991/gitsigns.nvim",
   opts = {
+    -- If I want the default which is a bit smaller, just delete `signs` and `signs_staged` tables
+    signs = {
+      add = { text = "▌" },
+      change = { text = "▌" },
+      delete = { text = "▌" },
+      topdelete = { text = "▌" },
+      changedelete = { text = "▌" },
+      untracked = { text = "▌" },
+    },
+    signs_staged = {
+      add = { text = "▌" },
+      change = { text = "▌" },
+      delete = { text = "▌" },
+      topdelete = { text = "▌" },
+      changedelete = { text = "▌" },
+    },
     current_line_blame = true,
   },
 }
