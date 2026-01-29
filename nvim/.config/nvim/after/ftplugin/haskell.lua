1f8ab37dc816f794b35b281433a4251e21981a7c
Simon Winther <simonwa01@gmail.com>
2026-01-29 21:50:54 +0100

    haskell new keys other shit wasn't working well XD

--- nvim/.config/nvim/after/ftplugin/haskell.lua
+++ nvim/.config/nvim/after/ftplugin/haskell.lua
@@ -4,29 +4,16 @@ local bufnr = vim.api.nvim_get_current_buf()
 local opts = { noremap = true, silent = true, buffer = bufnr }
 
 -- Hoogle search for the type signature of the definition under the cursor
-vim.keymap.set("n", "<space>hs", ht.hoogle.hoogle_signature, opts)
+vim.keymap.set(
+  "n",
+  "<localleader>t",
+  ht.hoogle.hoogle_signature,
+  vim.tbl_extend("force", opts, { desc = "Hoogle search for type signature" })
+)
 
--- Evaluate all code snippets
-vim.keymap.set("n", "<space>ea", ht.lsp.buf_eval_all, opts)
-
--- Reload right pane
-vim.keymap.set("n", "<leader>rr", function()
-  vim.cmd("w") -- save file
-  os.execute([[
-    if [ "$(tmux display-message -p '#{window_zoomed_flag}')" = "1" ]; then
-      tmux resize-pane -Z
-    fi
-    tmux select-pane -R \; send-keys ":r" Enter
-  ]])
-end, { noremap = true, silent = true, desc = "Reload right GHCi" })
-
--- Reload bottom pane
-vim.keymap.set("n", "<leader>rb", function()
+vim.keymap.set("n", "<localleader>s", function()
   vim.cmd("w")
-  os.execute([[
-    if [ "$(tmux display-message -p '#{window_zoomed_flag}')" = "1" ]; then
-      tmux resize-pane -Z
-    fi
-    tmux select-pane -D \; send-keys ":r" Enter
-  ]])
-end, { noremap = true, silent = true, desc = "Reload bottom GHCi" })
+  vim.cmd("botright split | resize 12 | terminal runghc %")
+  local buf = vim.api.nvim_get_current_buf()
+  vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = buf, silent = true })
+end, { buffer = true, desc = "Run Haskell file" })
