06763cdcc50f97d80fd00cad17caa8e2713f5dc9
Simon Winther <simonwa01@gmail.com>
2026-01-30 19:33:41 +0100

    lazy loading and unloading all functions

--- zsh/.config/zsh/functions/init.zsh
+++ zsh/.config/zsh/functions/init.zsh
@@ -1,11 +1,27 @@
-# Add this directory to fpath for autoloaded functions
-fpath=(${0:A:h} $fpath)
-
-# Autoload all functions in this dir (except init)
-# This is same as, manully doing:
+# What you're about to see below, is highly confidential and should not be
+# shared with anyone outside of the inner circle of zsh function developers.
+# Just kidding. The following below is a standard way to autoload all zsh
+# functions placed in the same directory as this init script.
+# Effectively it is the same as doing this:
 # fpath=(${0:A:h} $fpath)
 # autoload -Uz ez v vf gcd tldrf
-# Just cooler :D 
-autoload -Uz ${${(M)${(f)"$(print -l ${0:A:h}/*(:t))"}:#init}#*}
+# It is just more maintainable this way.
 
+# Define the directory and add to fpath
+local func_dir="${0:A:h}"
+fpath=($func_dir $fpath)
+typeset -U fpath
 
+# Get the list of all function files (excluding this init file)
+local files=($func_dir/*(N:t))
+local funcs=(${files:#init.zsh})
+
+# Forget the old versions so edits take effect on reload
+for f in $funcs; do
+  unfunction $f 2>/dev/null
+done
+
+# Mark them for lazy-loading
+if (( ${#funcs} )); then
+  autoload -Uz $funcs
+fi
