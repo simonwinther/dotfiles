2b59df4ab92bce5714bed5cb1193b2b5027dbd92
Simon Winther <simonwa01@gmail.com>
2026-01-30 19:34:01 +0100

    remove cc old, new cc is way better

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,14 +1,6 @@
 # ───────────── PATH ───────────── 
 alias lg='lazygit'
 
-# ───────────── Clipboard Copy ─────────────  
-# For X11
-if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
-    alias cc='wl-copy'
-else
-    alias cc='xclip -selection clipboard'
-fi
-
 # ───────────── Eza (better ls) ─────────────
 alias ls="eza --icons=always"
 alias ll="eza -la --icons=always"
