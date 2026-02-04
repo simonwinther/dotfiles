5d8f9e8f937ade4aca0381cffa07b0c2c5336247
Simon Winther <simonwa01@gmail.com>
2026-02-04 08:07:41 +0100

    plugin>presence.nvim:added

---
+++ nvim/.config/nvim/lua/plugins/presence.lua
@@ -0,0 +1,25 @@
+return {
+  "andweeb/presence.nvim",
+  lazy = false,
+  config = function()
+    require("presence").setup({
+      auto_update = true,
+      main_image = "file",
+      show_time = true,
+      debounce_timeout = 15,
+      buttons = true,
+
+      editing_text = "Editing %s",
+      reading_text = "Reading %s",
+      workspace_text = "In %s",
+      git_commit_text = "Writing a commit",
+
+      blacklist = {
+        "%.env",
+        "node_modules",
+        "/tmp",
+        "private",
+      },
+    })
+  end,
+}
