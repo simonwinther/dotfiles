5da522f6c5e0834fbec97287a2cee8680774043d
Simon Winther <simonwa01@gmail.com>
2026-02-09 09:15:50 +0100

    nvim>luasnip:hot reload

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -7,6 +7,15 @@ return {
       config = function()
         require("luasnip.loaders.from_vscode").lazy_load()
         require("luasnip.loaders.from_lua").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+
+        -- Hot Reload on Save
+        vim.api.nvim_create_autocmd("BufWritePost", {
+          pattern = "*/snippets/*.lua",
+          callback = function()
+            require("luasnip.loaders.from_lua").load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+            print("Snippets reloaded successfully!")
+          end,
+        })
       end,
     },
 
