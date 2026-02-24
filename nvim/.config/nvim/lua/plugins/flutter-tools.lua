7fcd329e9095da1a4d5e8b85be5770797d2ca07d
Simon Winther <simonwa01@gmail.com>
2026-02-24 22:42:37 +0100

    feat(nvim): add flutter-tools plugin configuration

---
+++ nvim/.config/nvim/lua/plugins/flutter-tools.lua
@@ -0,0 +1,17 @@
+return {
+  "akinsho/flutter-tools.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim",
+  },
+  config = function()
+    require("flutter-tools").setup({
+      lsp = {
+        color = {
+          enabled = true,
+        },
+      },
+    })
+  end,
+}
