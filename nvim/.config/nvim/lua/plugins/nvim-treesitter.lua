04667a08a94713427374904de5ba2df6ca0dceec
Simon Winther <simonwa01@gmail.com>
2026-03-04 16:30:23 +0100

    feat(nvim): add treesitter-modules with incremental selection keymaps

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,27 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    local ensure_installed = opts.ensure_installed
-    vim.list_extend(ensure_installed, { "latex", "bibtex", "nix", "hyprlang" })
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+  },
+  {
+    {
+      "MeanderingProgrammer/treesitter-modules.nvim",
+      dependencies = { "nvim-treesitter/nvim-treesitter" },
+      opts = {
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<A-o>",
+            node_incremental = "<A-o>",
+            scope_incremental = "<A-O>",
+            node_decremental = "<A-i>",
+          },
+        },
+      },
+    },
+    -- keys = {
+    --   { "<C-space>", false, mode = { "n", "x" } },
+    -- },
+  },
 }
