6c94646dceaa1a50ee776ca5e9f067fde007e1b1
Simon Winther <simonwa01@gmail.com>
2026-05-01 17:03:28 +0200

    feat(nvim): add cppman plugin and refresh lazy lockfile

---
+++ nvim/.config/nvim/lua/plugins/cppman.lua
@@ -0,0 +1,14 @@
+return {
+  dir = "~/dev/nvim/cppman.nvim",
+  opts = {
+    source = "both",
+    viewer = {
+      width = 0.94,
+      height = 0.75,
+    },
+    picker = {
+      width = 0.4,
+      height = 0.3,
+    },
+  },
+}
