5a49fb1d14879cf00cfdd46367e0406dcd073b35
Simon Winther <simonwa01@gmail.com>
2026-05-11 23:12:46 +0200

    feat(nvim): add CPPMan commands and key mappings for C++ support

--- nvim/.config/nvim/lua/plugins/cppman.lua
+++ nvim/.config/nvim/lua/plugins/cppman.lua
@@ -1,5 +1,22 @@
 return {
   dir = "~/dev/nvim/cppman.nvim",
+  cmd = "CPPMan",
+  keys = {
+    {
+      "<leader>cu",
+      function()
+        require("cppman").open_for(vim.fn.expand("<cword>"))
+      end,
+      desc = "[C++] open under cursor",
+    },
+    {
+      "<leader>ck",
+      function()
+        require("cppman").search()
+      end,
+      desc = "[C++] keyword search",
+    },
+  },
   opts = {
     source = "both",
     viewer = {
