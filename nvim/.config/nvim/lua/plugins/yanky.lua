9628849c461344546f326cde5bbd6fb9a2b49fc0
Simon Winther <simonwa01@gmail.com>
2026-05-11 13:34:20 +0200

    feat(nvim): add new keybindings for yanky plugin functionality

--- nvim/.config/nvim/lua/plugins/yanky.lua
+++ nvim/.config/nvim/lua/plugins/yanky.lua
@@ -26,5 +26,26 @@ return {
       mode = { "n", "x" },
       desc = "[S]earch [Y]ank History",
     },
+    { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" } },
+    { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" } },
+    { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" } },
+    { "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" } },
+
+    { "<C-p>", "<Plug>(YankyPreviousEntry)", mode = "n" },
+    { "<C-n>", "<Plug>(YankyNextEntry)", mode = "n" },
+
+    -- tpope/vim-unimpaired style
+    { "]p", "<Plug>(YankyPutIndentAfterLinewise)", mode = "n" },
+    { "[p", "<Plug>(YankyPutIndentBeforeLinewise)", mode = "n" },
+    { "]P", "<Plug>(YankyPutIndentAfterLinewise)", mode = "n" },
+    { "[P", "<Plug>(YankyPutIndentBeforeLinewise)", mode = "n" },
+
+    { ">p", "<Plug>(YankyPutIndentAfterShiftRight)", mode = "n" },
+    { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", mode = "n" },
+    { ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", mode = "n" },
+    { "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", mode = "n" },
+
+    { "=p", "<Plug>(YankyPutAfterFilter)", mode = "n" },
+    { "=P", "<Plug>(YankyPutBeforeFilter)", mode = "n" },
   },
 }
