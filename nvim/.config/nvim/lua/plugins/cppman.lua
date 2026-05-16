eef1b10bad1da8a3d5c13272325bbfca9d178ef1
Simon Winther <simonwa01@gmail.com>
2026-05-16 20:20:41 +0200

    chore(nvim): update lazy-lock.json and cppman plugin configuration

--- nvim/.config/nvim/lua/plugins/cppman.lua
+++ nvim/.config/nvim/lua/plugins/cppman.lua
@@ -1,5 +1,6 @@
 return {
-  dir = "~/dev/nvim/cppman.nvim",
+  "simonwinther/cppman.nvim",
+  version = "*",
   cmd = "CPPMan",
   keys = {
     {
@@ -17,15 +18,7 @@ return {
       desc = "[C++] keyword search",
     },
   },
-  opts = {
-    source = "both",
-    viewer = {
-      width = 0.94,
-      height = 0.75,
-    },
-    picker = {
-      width = 0.4,
-      height = 0.3,
-    },
+  dependencies = {
+    "folke/snacks.nvim",
   },
 }
