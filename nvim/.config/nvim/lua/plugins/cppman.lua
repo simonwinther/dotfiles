1e59b4df19c781e805180a2bd89c944011d7bbb4
Simon Winther <simonwa01@gmail.com>
2026-06-06 16:47:31 +0200

    feat(cppman): add development mode and separate command for testing

--- nvim/.config/nvim/lua/plugins/cppman.lua
+++ nvim/.config/nvim/lua/plugins/cppman.lua
@@ -1,24 +1,50 @@
+local dev_mode = true
+
 return {
-  "simonwinther/cppman.nvim",
-  version = "*",
-  cmd = "CPPMan",
-  keys = {
-    {
-      "<localleader>w",
-      function()
-        require("cppman").open_for(vim.fn.expand("<cword>"))
-      end,
-      desc = "[C++] open under cursor",
-    },
-    {
-      "<localleader>s",
-      function()
-        require("cppman").search()
-      end,
-      desc = "[C++] keyword search",
+  {
+    dir = "~/dev/nvim/cppman.nvim",
+    name = "cppman.nvim-dev",
+    cmd = "CPPManDev",
+    enabled = dev_mode,
+    keys = {
+      {
+        "<localleader>w",
+        function()
+          require("cppman").open_for(vim.fn.expand("<cword>"))
+        end,
+        desc = "DEV: [C++] open under cursor",
+      },
+      {
+        "<localleader>s",
+        function()
+          require("cppman").search()
+        end,
+        desc = "DEV: [C++] keyword search",
+      },
     },
   },
-  dependencies = {
-    "folke/snacks.nvim",
+
+  {
+    "simonwinther/cppman.nvim",
+    name = "cppman.nvim",
+    version = "*",
+    cmd = "CPPMan",
+    enabled = not dev_mode,
+    keys = {
+      {
+        "<localleader>w",
+        function()
+          require("cppman").open_for(vim.fn.expand("<cword>"))
+        end,
+        desc = "[C++] open under cursor",
+      },
+      {
+        "<localleader>s",
+        function()
+          require("cppman").search()
+        end,
+        desc = "[C++] keyword search",
+      },
+    },
   },
 }
