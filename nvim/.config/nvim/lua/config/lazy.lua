a76e6548c5331b1adaa0977679dbf363e58b6382
Simon Winther <simonwa01@gmail.com>
2026-06-15 18:42:34 +0200

    feat(plugins): enable development mode for cppman.nvim plugin

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -23,6 +23,8 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "plugins" },
   },
+  -- Where locally-developed plugins live (used by specs with `dev = true`).
+  dev = { path = "~/dev/nvim" },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
--- nvim/.config/nvim/lua/plugins/cppman.lua
+++ nvim/.config/nvim/lua/plugins/cppman.lua
@@ -1,46 +1,27 @@
-local dev_mode = false
+local dev_mode = true
 
 return {
-  {
-    dir = "~/dev/nvim/cppman.nvim",
-    name = "cppman.nvim-dev",
-    cmd = "CPPManDev",
-    enabled = dev_mode,
-    init = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "cpp", "c" },
-        callback = function(args)
-          vim.keymap.set("n", "<localleader>w", function()
-            require("cppman").open_for(vim.fn.expand("<cword>"))
-          end, { buffer = args.buf, desc = "DEV: [C++] open under cursor" })
-
-          vim.keymap.set("n", "<localleader>s", function()
-            require("cppman").search()
-          end, { buffer = args.buf, desc = "DEV: [C++] keyword search" })
-        end,
-      })
-    end,
+  "simonwinther/cppman.nvim",
+  dev = dev_mode,
+  version = "*",
+  cmd = "CPPMan",
+  opts = {
+    viewer = {
+      border = "single",
+    },
   },
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "cpp", "c" },
+      callback = function(args)
+        vim.keymap.set("n", "<localleader>w", function()
+          require("cppman").open_for(vim.fn.expand("<cword>"))
+        end, { buffer = args.buf, desc = "[C++] open under cursor" })
 
-  {
-    "simonwinther/cppman.nvim",
-    name = "cppman.nvim",
-    version = "*",
-    cmd = "CPPMan",
-    enabled = not dev_mode,
-    init = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "cpp", "c" },
-        callback = function(args)
-          vim.keymap.set("n", "<localleader>w", function()
-            require("cppman").open_for(vim.fn.expand("<cword>"))
-          end, { buffer = args.buf, desc = "[C++] open under cursor" })
-
-          vim.keymap.set("n", "<localleader>s", function()
-            require("cppman").search()
-          end, { buffer = args.buf, desc = "[C++] keyword search" })
-        end,
-      })
-    end,
-  },
+        vim.keymap.set("n", "<localleader>s", function()
+          require("cppman").search()
+        end, { buffer = args.buf, desc = "[C++] keyword search" })
+      end,
+    })
+  end,
 }
