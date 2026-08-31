8a2017cff110bf8cc287a28b85dcfa3ea4ed17f1
Simon Winther <simonwa01@gmail.com>
2026-08-31 13:41:58 +0200

    feat(nvim): add C and C++ specific settings for indentation and style

---
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -0,0 +1,12 @@
+-- =============================
+-- C specific settings i try to keep in sync with ~/.clang-format
+-- =============================
+
+vim.opt_local.indentexpr = ""
+
+vim.opt_local.cindent = true
+
+vim.opt_local.expandtab = true
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.tabstop = 4
