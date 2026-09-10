4baf5415ff70d815e50d4dc03faaa7257eb4381a
Simon Winther <simonwa01@gmail.com>
2026-09-10 22:21:32 +0200

    chore(nvim): update dev path for local plugins configuration

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -24,7 +24,7 @@ require("lazy").setup({
     { import = "plugins" },
   },
   -- Where locally-developed plugins live (used by specs with `dev = true`).
-  dev = { path = "~/dev/nvim" },
+  dev = { path = "~/dev/system/nvim" },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
